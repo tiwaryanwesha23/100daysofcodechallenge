4ae85fac954e9eef21b8d450693d99c5525c3457
ANWESHA TIWARY <86918964+tiwaryanwesha23@users.noreply.github.com>
2023-04-13 20:20:57 +0530

    Time: 0 ms (100.00%), Space: 41 MB (70.32%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        int faster, slower = 0, size = nums.length, length = size;
+
+        for (faster = 0; faster < size; faster++) {
+            if (nums[faster] != val) nums[slower++] = nums[faster];
+            else length--;
+        }
+
+        return length;
+    }
+}
