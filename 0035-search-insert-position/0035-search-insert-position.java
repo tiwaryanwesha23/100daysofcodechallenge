4d67a60e38c5307555d3167e3cab66c21981518d
ANWESHA TIWARY <86918964+tiwaryanwesha23@users.noreply.github.com>
2023-05-05 08:56:08 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (48.98%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int h = 0;
+        int t = nums.length - 1;
+        int m = ( h + t ) / 2;
+        
+        while( h <= t ){
+            m = ( h + t ) / 2;
+            if( nums[m] == target ){
+                return m;
+            }else if( nums[m] < target ){
+                h = m + 1;
+            }else{
+                t = m - 1;
+            }
+        }
+        return h;
+        // return target > nums[m]  ? m+1 : m;
+    }
+}
