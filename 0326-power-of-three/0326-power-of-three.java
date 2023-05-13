91818d0f8dde4b4ab09a08cdf73d5ced5505098c
ANWESHA TIWARY <86918964+tiwaryanwesha23@users.noreply.github.com>
2023-05-13 23:12:31 +0530

    Time: 15 ms (99.88%), Space: 42.4 MB (64.92%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public boolean isPowerOfThree(int n) {
+        if( n == 0 ) return false;
+        int r = 0;
+        
+        while( n > 1 ){
+            r = n % 3;
+            if( r != 0 ) return false;
+            n = n / 3;
+        }
+        return n == 1 ?  true : false;
+    }
+}
