6c2985fa24573db6bbc3915121b55f0d4eb69400
ANWESHA TIWARY <86918964+tiwaryanwesha23@users.noreply.github.com>
2023-05-28 11:01:04 +0530

    Time: 31 ms (41.65%), Space: 44.9 MB (7.28%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+         int array[] = new int[26];
+        int len = tasks.length;
+        for (int i = 0; i < len; i++) {
+            array[tasks[i] - 'A']++;
+        }
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>((a, b) -> b - a);
+        for (int i = 0; i < 26; i++) {
+            if (array[i] != 0) {
+                queue.offer(array[i]);
+            }
+        }
+        int count = 0;
+        while (!queue.isEmpty()) {
+            List<Integer> list = new ArrayList<>();
+            int tempCount = 0;
+            for (int i = 0; i <= n; i++) {
+                if (!queue.isEmpty()) {
+                    list.add(queue.poll());
+                    tempCount++;
+                }
+            }
+            for (Integer num : list) {
+                if (num - 1 > 0) {
+                    queue.offer(num - 1);
+                }
+            }
+            tempCount = !queue.isEmpty() ? n + 1 : tempCount;
+            count += tempCount;
+        }
+        return count;
+    }
+}
