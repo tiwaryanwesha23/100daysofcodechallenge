3af9443762bc5df01a8cee66adc97a0ee2dd8e5a
ANWESHA TIWARY <86918964+tiwaryanwesha23@users.noreply.github.com>
2023-06-06 10:43:11 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (82.21%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return dfs(root.left, root.right);
+    }
+    
+    private boolean dfs(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null) return false;
+        return left.val == right.val
+            && dfs(left.right, right.left)
+            && dfs(left.left, right.right);
+    }
+}
