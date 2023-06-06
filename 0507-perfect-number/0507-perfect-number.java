4f98c205f59c520c4150faeaab2ff4e87c297e66
ANWESHA TIWARY <86918964+tiwaryanwesha23@users.noreply.github.com>
2023-06-06 10:47:18 +0530

    Time: 1406 ms (15.36%), Space: 39.6 MB (37.62%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        if (num < 6 || num%2 != 0) {
+            return false;
+        }
+        int sum = 1;
+        for (int i=2; i<=num/2; i++) {
+            if (num%i == 0) {
+                sum += i;
+            }
+        }
+        return sum == num;
+    }
+}
