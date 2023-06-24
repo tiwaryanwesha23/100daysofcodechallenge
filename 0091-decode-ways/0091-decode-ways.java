f9d2a50fbb837327f379915ecd25fed0a63afb4e
ANWESHA TIWARY <86918964+tiwaryanwesha23@users.noreply.github.com>
2023-06-24 12:43:48 +0530

    Time: 2 ms (28.54%), Space: 41 MB (57.42%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numDecodings(String s) {
+        int[] dp = new int[s.length() + 1];
+        
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+        
+        for (int i = 2; i < dp.length; ++i) {
+            if (s.charAt(i - 1) != '0') {
+                dp[i] += dp[i-1];
+            }
+            
+            int two_pre = Integer.valueOf(s.substring(i-2, i));
+            
+            if(two_pre >= 10 && two_pre <= 26) {
+                dp[i] += dp[i-2];
+            }
+        }
+        return dp[s.length()];
+    }
+}
