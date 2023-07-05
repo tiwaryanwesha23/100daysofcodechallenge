67748105b9ffbbb01f387fa7284c93edd78cb1b4
ANWESHA TIWARY <86918964+tiwaryanwesha23@users.noreply.github.com>
2023-07-05 14:11:49 +0530

    Time: 5 ms (71.79%), Space: 44 MB (41.07%) - LeetHub

---
+++ 0748-shortest-completing-word/0748-shortest-completing-word.java
@@ -0,0 +1,44 @@
+class Solution {
+    public String shortestCompletingWord(String licensePlate, String[] words) {
+        int[] chNum = new int[26];
+        for (char ch : licensePlate.toCharArray()) {
+            if (isLetter(ch)) {
+                char tmp = Character.toLowerCase(ch);
+                chNum[tmp - 'a']++;
+            }
+        }
+
+        String res = null;
+        for (String w : words) {
+            String tmp = w.toLowerCase();
+            int[] chNumTmp = Arrays.copyOf(chNum, 26);
+            for (char ch : tmp.toCharArray()) {
+                chNumTmp[ch - 'a']--;
+            }
+
+            boolean ok = true;
+            for (int n : chNumTmp) {
+                if (n > 0) {
+                    ok = false;
+                    break;
+                }
+            }
+
+            if (ok && (res == null || res.length() > w.length())) {
+                res = w;
+            }
+        }
+
+        return res;
+    }
+
+    private boolean isLetter(char ch) {
+        ch = Character.toLowerCase(ch);
+        int tmp = ch - 'a';
+        if (tmp >= 0 && tmp < 26) {
+            return true;
+        }
+
+        return false;
+    }
+}
