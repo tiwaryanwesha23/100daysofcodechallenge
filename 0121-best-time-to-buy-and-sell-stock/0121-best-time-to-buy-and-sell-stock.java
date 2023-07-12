a60cf0d8606e1bca0c4371c44aa8c80fc273cfec
ANWESHA TIWARY <86918964+tiwaryanwesha23@users.noreply.github.com>
2023-07-12 23:52:43 +0530

    Time: 2 ms (93.52%), Space: 60.9 MB (71.76%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,15 +1,11 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        
         //In constraints it is given that 
         //0 <= prices[i] <= 104
         int min = 10000;
-        
         //Profit will be 0, if no transaction are done.
         int maxDiff = 0;
-        
         int size = prices.length;
-        
         for (int i = 0; i < size; i++){
             //We need to find Min value
             min = Math.min(prices[i], min);
